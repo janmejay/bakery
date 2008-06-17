69a462c97ce595790ed34dad55e748f1cafb63ad
Janmejay Singh <singh.janmejay@gmail.com>
2008-06-17 09:04:58 +0530

    Initializing with a working tutorial gosu file.... on top of gosu

---
+++ src/init.rb
@@ -0,0 +1,9 @@
+# User: janmejay.singh
+# Time: 16 Jun, 2008 8:11:31 PM
+
+unless defined?(BAKERY_CONFIGURATION_DEFINED)
+  BAKERY_CONFIGURATION_DEFINED = true
+  $LOAD_PATH << File.dirname(__FILE__) + '/../lib/x86-linux' if RUBY_PLATFORM =~ /86-linux/
+  $LOAD_PATH << File.dirname(__FILE__) + '/../lib/x86_64-linux' if RUBY_PLATFORM =~ /86_64-linux/
+  $LOAD_PATH << File.dirname(__FILE__)
+end
