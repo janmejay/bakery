bdbbf40055db42c0bc4ba486b39db6d24d0c3445
Janmejay Singh <singh.janmejay@gmail.com>
2008-06-21 08:05:00 +0530

    added baker's walking area sanatization(so that she doesn't step over the table and stuff....)

--- src/baker.rb
+++ src/baker.rb
@@ -2,12 +2,30 @@
 # Time: 19 Jun, 2008 6:03:53 PM
 class Baker
 
+  class LimitingRectangle
+    def initialize top_left_x, top_left_y, bottom_right_x, bottom_right_y
+      @min_x = top_left_x
+      @min_y = top_left_y
+      @max_x = bottom_right_x
+      @max_y = bottom_right_y
+    end
+
+    def sanatize x, y
+      x = @min_x if x < @min_x
+      y = @min_y if y < @min_y
+      x = @max_x if x > @max_x
+      y = @max_y if y > @max_y
+      return x, y
+    end
+  end
+
   VELOCITY = 2
 
   def initialize window
     @body = Gosu::Image.new(window, "media/baker.png", false)
     @window = window
     @x, @y, @target_x, @target_y, @angle = 0, 0, 0, 0, 0, 0, 0
+    @sane_walking_area = LimitingRectangle.new(384, 178, 835, 550)
   end
 
   def pointed_to x_cord, y_cord
@@ -22,6 +40,7 @@ class Baker
     dy = Gosu::offset_y(@angle, VELOCITY)
     @x += dx
     @y += dy
+    @x, @y = @sane_walking_area.sanatize @x, @y
   end
 
   def draw
