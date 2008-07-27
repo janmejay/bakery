820656dc40a4d79364df41d52967b3c0cc65d6e9
Janmejay Singh <singh.janmejay@gmail.com>
2008-07-27 11:30:08 -0400

    made the baker start from the center from the room .... instead of the top left corner

--- src/baker.rb
+++ src/baker.rb
@@ -33,7 +33,7 @@ class Baker
     @walking_anim = Util::Animator.new(window, 'media/walking_baker.png', 105, 80, false, 2, true)
     @hat = Gosu::Image.new(window, 'media/baker_hat.png', false)
     @window = window
-    @x, @y, @target_x, @target_y, @angle = 0, 0, 0, 0, 0, 0, 0
+    @x, @y, @target_x, @target_y, @angle = 600, 400, 600, 400, 180
     @sane_walking_area = LimitingRectangle.new(384, 200, 835, 550)
   end
   
