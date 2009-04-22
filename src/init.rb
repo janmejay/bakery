d35c5b2a071332d02d91ec3417f8c8446191def5
Janmejay Singh <singh.janmejay@gmail.com>
2009-04-22 17:56:04 +0530

    fixed issue 1 http://github.com/janmejay/bakery/issues#issue/1

--- src/init.rb
+++ src/init.rb
@@ -16,6 +16,7 @@ $LOAD_PATH << File.dirname(__FILE__)
 $BAKERY_HOME = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 $BAKERY_TMP = File.join($BAKERY_HOME, 'tmp')
 
+
 $PLAYER_DATA_BASE_PATH = File.join($BAKERY_TMP, '#name#')
 $BAKERY_FILE_EXT = '.bkry'
 $LAST_PLAYER_FILE_PATH = File.join($BAKERY_TMP, "last_played_by#{$BAKERY_FILE_EXT}.bkr")
@@ -23,8 +24,6 @@ $SAVED_GAMES_DIR = File.join($BAKERY_TMP, '#name#', 'saved_games')
 $LAST_PLAYED_GAME_NAME = File.join($BAKERY_TMP, '#name#', 'last_played')
 $SAVED_FILE_EXT = '.frz'
 $LEVEL_FILE_EXT = '.lvl'
-
-File.exist?($LAST_PLAYER_FILE_PATH) || File.open($LAST_PLAYER_FILE_PATH, 'w') { |h| h.write('Sweta') }
 
 def resource path
   File.join($BAKERY_HOME, path)
@@ -40,6 +39,7 @@ require 'logger'
 require File.join('util', 'util')
 require 'fileutils'
 FileUtils.mkdir_p($BAKERY_TMP)
+File.exist?($LAST_PLAYER_FILE_PATH) || File.open($LAST_PLAYER_FILE_PATH, 'w') { |h| h.write('Sweta') }
 
 $logger = Logger.new($BAKERY_LOG_FILE = File.join($BAKERY_HOME, 'tmp', 'bakery.log'))
 $logger.level = Logger::DEBUG
