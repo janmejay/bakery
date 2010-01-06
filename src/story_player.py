43dcb812ce16bf9da92848982e44222e90c9ce3a
Janmejay Singh <singh.janmejay@gmail.com>
2010-01-06 00:20:43 -0800

    story player now loads story images for level

--- src/story_player.py
+++ src/story_player.py
@@ -1,4 +1,18 @@
 import bakery_wizard
+import os
+from util import game_util
 
 class StoryPlayer(bakery_wizard.BaseWindow):
-    pass
+    MONTHS = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+
+    def load(self, screen):
+        bakery_wizard.BaseWindow.load(self, screen)
+        month_dir = os.path.join('stories', '%s') % StoryPlayer.MONTHS[self.bakery_wizard.context['level'] - 1]
+        story_file_names = os.listdir(game_util.media(month_dir))
+        story_file_names.sort()
+        self.story_screens = [ game_util.load_image(os.path.join(month_dir, file_name)) for file_name in story_file_names ]
+        
+        
+        
+        
+
--- test/story_player_test.py
+++ test/story_player_test.py
@@ -3,15 +3,35 @@ import env
 import story_player
 import bakery_wizard
 import pygame
+from util import game_util
+import mox
 
 class StoryPlayer(unittest.TestCase):
     def setUp(self):
         self.screen = pygame.surface.Surface((10, 10))
         self.wizard = bakery_wizard.BakeryWizard()
+        self.wizard.context['level'] = 1
         self.window = story_player.StoryPlayer(self.wizard)
 
     def test_is_base_window(self):
         self.assertTrue(isinstance(self.window, bakery_wizard.BaseWindow))
+
+    def test_loads_images_for_the_corresponding_month(self):
+        self.assertFalse(hasattr(self.window, 'story_screens'))
+        mock_factory = mox.Mox()
+        mock_factory.StubOutWithMock(game_util, 'load_image')
+        image_1 = pygame.surface.Surface((10, 10))
+        image_2 = pygame.surface.Surface((10, 10))
+        image_3 = pygame.surface.Surface((10, 10))
+
+        game_util.load_image('stories/jan/1.png').AndReturn(image_1)
+        game_util.load_image('stories/jan/2.png').AndReturn(image_2)
+        game_util.load_image('stories/jan/3.png').AndReturn(image_3)
+        mock_factory.ReplayAll()
+        self.window.load(self.screen);
+        mock_factory.VerifyAll()
+        self.assertEqual(self.window.story_screens, [image_1, image_2, image_3])
+
 
 if __name__ == '__main__':
     unittest.main()
